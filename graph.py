f738e190c3ebad43ff7c2cbc6b04cb1f2ea4428f
root <root@LAPTOP-TIC2VA1F.localdomain>
2021-03-26 18:38:57 +0300

    update graph

--- graph.py
+++ graph.py
@@ -11,13 +11,19 @@ class Graph:
         self.nodes.update({node.id: node})
 
     def connect(self, node1: int, node2: int) -> None:
-        if node1 not in self.nodes.keys or node2 not in self.nodes.keys:
+        if node1 not in self.nodes.keys() or node2 not in self.nodes.keys():
             raise Exception("nodes not in graph")
 
         e = Edge(self.nodes[node1], self.nodes[node2])
         self.edges.append(e)
         self.nodes[node1].connectedEdges.append(e)
         self.nodes[node2].connectedEdges.append(e)
+
+    def get_dic_nodes(self):
+        return self.nodes
+
+    def get_list_edges(self):
+        return self.edges
 
     def num_of_nodes(self) -> int:
         return len(self.nodes)
--- graph_utils.py
+++ graph_utils.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Node:
-    __idCounter = 1
+    __idCounter = 0
 
     def __init__(self):
         self.id: int = Node.__idCounter
@@ -10,10 +10,38 @@ class Node:
         self.connectedEdges: List[Edge] = []
         self.tag1: any = None  # for algorithms
         self.tag2: any = None  # for algorithms
+        self.tag3: any = None  # for algorithms
+        self.tag4: any = None  # for algorithms
 
+    def get_list_edge(self):
+        return self.connectedEdges
+
+    def set_tag1(self, tag: any = None):
+        self.tag1 = tag
+
+    def set_tag2(self, tag: any = None):
+        self.tag2 = tag
+
+    def get_tag1(self):
+        return self.tag1
+
+    def get_tag2(self):
+        return self.tag2
+
+    def __str__(self):
+        return "The ID is: "  + str(self.id)
+
+    def __repr__(self):
+        return "The ID is: "  + str(self.id)
 
 class Edge:
 
     def __init__(self, src: Node, dest: Node):
         self.src = src
         self.dest = dest
+
+    def get_src(self):
+        return self.src
+
+    def get_dest(self):
+        return self.dest
